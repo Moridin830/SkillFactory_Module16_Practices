c1d15b1155568d440657c55621d1273b94e0d1f0
Виктор Редькин <Moridin830@gmail.com>
2022-03-03 20:03:08 +0300

    Добавьте файлы проекта.

---
+++ Practices.Tests/CalculatorTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Practices.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void Additional_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Additional(1, 2), Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Subtraction_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Subtraction(2, 1), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Miltiplication_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Miltiplication(2, 3), Is.EqualTo(6));
+        }
+
+        [Test]
+        public void Division_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Division(6, 3), Is.EqualTo(2));
+        }
+    }
+}
---
+++ PracticesTests/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace PracticesTests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void Additional_MustReturnCorrectValue(int a, int b)
+        {
+            CalculatorTests
+        }
+    }
+}
